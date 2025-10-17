e3ad90ae4844eecce8eed8b4c398936462fb534d
José Pernías <jcpernias@users.noreply.github.com>
2025-10-17 11:47:07 +0200

    Rename file

--- read-dgt-data.R
+++ read-dgt-data.R
@@ -13,31 +13,37 @@ dgt_dict <- read_csv("data/dgt_mun_dict.csv", col_types = "icc") |>
   mutate(dgt_col_types = case_when(
     is.na(var) ~ "skip",
     type == "c" ~ "text",
-    .default = "numeric"))
+    .default = "numeric"
+  ))
 
 # Funciones para leer los datos de un año
 drop_na <- function(x) {
   x[!is.na(x)]
 }
 
-read_dgt_file <- function(year, input_file, dict)
-{
+read_dgt_file <- function(year, input_file, dict) {
   # El año 2021 hay una variable menos
-  if(year == 2021)
+  if (year == 2021) {
     dict <- dict |> filter(column != 57)
+  }
 
   read_xlsx(input_file,
-            skip = 1,
-            col_types = dict$dgt_col_types,
-            col_names = drop_na(dict$var)) |>
+    skip = 1,
+    col_types = dict$dgt_col_types,
+    col_names = drop_na(dict$var)
+  ) |>
     filter(!str_detect(mun, fixed("sin especificar"))) |>
     mutate(year = year)
 }
 
 # Lee los datos de todos los años
-dgt_data <- pmap(dgt_files,
-                 \(year, file) read_dgt_file(year, file, dgt_dict)) |>
+dgt_data <- pmap(
+  dgt_files,
+  \(year, file) read_dgt_file(year, file, dgt_dict)
+) |>
   bind_rows()
+
+saveRDS(dgt_data, file = "data/dgt_mun_data.rds", compress = "xz")
 
 # Determina los municipios poco poblados
 low_pop_thresh <- 1000
@@ -55,16 +61,20 @@ incr <- dgt_smpl |>
   filter(year %in% c(2021, 2024)) |>
   group_by(ine_code) |>
   arrange(year) |>
-  mutate(incr_num_all = num_all - lag(num_all),
-         incr_badge_0 = badge_0 - lag(badge_0),
-         incr_badge_eco = badge_eco - lag(badge_eco),
-         incr_badge_c = badge_c - lag(badge_c),
-         incr_badge_b = badge_b - lag(badge_b),
-         incr_badge_none = badge_none - lag(badge_none)) |>
+  mutate(
+    incr_num_all = num_all - lag(num_all),
+    incr_badge_0 = badge_0 - lag(badge_0),
+    incr_badge_eco = badge_eco - lag(badge_eco),
+    incr_badge_c = badge_c - lag(badge_c),
+    incr_badge_b = badge_b - lag(badge_b),
+    incr_badge_none = badge_none - lag(badge_none)
+  ) |>
   ungroup() |>
   filter(year == 2024) |>
-  select(ine_code, incr_num_all, incr_badge_0, incr_badge_eco, incr_badge_c,
-         incr_badge_b, incr_badge_none)
+  select(
+    ine_code, incr_num_all, incr_badge_0, incr_badge_eco, incr_badge_c,
+    incr_badge_b, incr_badge_none
+  )
 
 
 incr |>
