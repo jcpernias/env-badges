b34a224d2f242d68c0b2105a9fb95b09a1907b41
José Pernías <jcpernias@users.noreply.github.com>
2025-10-22 18:39:59 +0200

    Move code to another file

---
+++ env-badges.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+
+
+dgt_data <- readRDS("data/dgt_mun_data.rds")
+
+# Determina los municipios poco poblados
+low_pop_thresh <- 1000
+
+low_pop <- dgt_data |>
+  summarise(min_pop = min(pop_total), .by = ine_code) |>
+  filter(min_pop < low_pop_thresh)
+
+dgt_smpl <- dgt_data |>
+  anti_join(low_pop, by = "ine_code")
+
+# Incrementos del parque y de los distintivos
+
+incr <- dgt_smpl |>
+  filter(year %in% c(2021, 2024)) |>
+  group_by(ine_code) |>
+  arrange(year) |>
+  mutate(
+    incr_num_all = num_all - lag(num_all),
+    incr_badge_0 = badge_0 - lag(badge_0),
+    incr_badge_eco = badge_eco - lag(badge_eco),
+    incr_badge_c = badge_c - lag(badge_c),
+    incr_badge_b = badge_b - lag(badge_b),
+    incr_badge_none = badge_none - lag(badge_none)
+  ) |>
+  ungroup() |>
+  filter(year == 2024) |>
+  select(
+    ine_code, incr_num_all, incr_badge_0, incr_badge_eco, incr_badge_c,
+    incr_badge_b, incr_badge_none
+  )
+
+
+incr |>
+  summarise(na_all = is.na(sum(c(incr_num_all))), .by = ine_code) |>
+  filter(na_all)
+
+incr |>
+  filter(!is.na(incr_num_all)) |>
+  select(-ine_code) |>
+  cor()
